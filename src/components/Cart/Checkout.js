cdf72bf7a7429f77c66142156ff0ebaef28a41ca
Michael Manlulu <michael.manlulu@myresman.com>
2022-06-24 14:50:21 +0800

    220. reading form values

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,31 +1,45 @@
+import { useRef } from 'react';
 import classes from './Checkout.module.css';
+
 const Checkout = (props) => {
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalInputRef = useRef();
+  const cityInputRef = useRef();
+
   const confirmHandler = (event) => {
     event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostal = postalInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
   };
 
   return (
-    <form onSubmit={confirmHandler}>
+    <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
-        <label htmlFor='name'>You Name</label>
-        <input type='text' id='name' />
+        <label htmlFor='name'>Your Name</label>
+        <input type='text' id='name' ref={nameInputRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor='name'>Street</label>
-        <input type='text' id='street' />
+        <label htmlFor='street'>Street</label>
+        <input type='text' id='street' ref={streetInputRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor='name'>Postal Code</label>
-        <input type='text' id='postal' />
+        <label htmlFor='postal'>Postal Code</label>
+        <input type='text' id='postal' ref={postalInputRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor='name'>City</label>
-        <input type='text' id='city' />
+        <label htmlFor='city'>City</label>
+        <input type='text' id='city' ref={cityInputRef} />
       </div>
-      <button type='button' onClick={props.onCancel}>
-        Cancel
-      </button>
-      <button type='submit'>Confirm</button>
+      <div className={classes.actions}>
+        <button type='button' onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
